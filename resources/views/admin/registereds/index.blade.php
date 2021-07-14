d7e4ed30a0ac3e9292a817595aeca92acdec66b9
Hossam Hassan <hossamhassan889@gmail.com>
2021-07-14 11:52:13 +0200

    g

--- resources/views/admin/registereds/index.blade.php
+++ resources/views/admin/registereds/index.blade.php
@@ -179,7 +179,7 @@
     ],
     orderCellsTop: true,
     order: [[ 1, 'desc' ]],
-    pageLength: 100,
+    pageLength: 5000,
   };
   let table = $('.datatable-Registered').DataTable(dtOverrideGlobals);
   $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
